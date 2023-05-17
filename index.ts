5538be93c01461ca1cc32ab42f2c0243bd4a5ced
markos <markos.paltser@voco.ee>
2023-05-17 10:34:11 +0300

    connect to database

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 import express, { Express, Request, Response } from "express";
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://kristjanpuusepp303:ftl1BRIz8BItD2YK@cluster0.mxusupf.mongodb.net/");
+mongoose.connect("mongodb+srv://kristjanpuusepp303:ftl1BRIz8BItD2YK@cluster0.mxusupf.mongodb.net/pood");
 const database = mongoose.connection;
 
 database.on('error', (error) => {
