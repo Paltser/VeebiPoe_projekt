0033a58d46ec67c757981a380722e18eaba32ea5
markos <markos.paltser@voco.ee>
2023-05-17 10:26:26 +0300

    Innitial commit

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
+// @ts-ignore
 import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://kristjanpuusepp303:ftl1BRIz8BItD2YK@cluster0.mxusupf.mongodb.net/");
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
 
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.send('Your web app is running on a Node');
 });
 
 
